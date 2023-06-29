8e2c51fc81a317d03b138f299708cfc2b7a1f854
Hadrien Huvelle <hhu@baboum.be>
2023-06-29 22:22:07 +0200

    remove  || exit 0 from doscker-entrypoint

--- 9.0/docker-entrypoint.sh
+++ 9.0/docker-entrypoint.sh
@@ -14,8 +14,7 @@ echo ${NGX_SPECIFIC_SERVER_CONFIG:-"#NO specific configuration defined"} > /etc/
 if [[ -n "${NGX_PROMETHEUS_EXPORTER}" ]]
 then
     echo "access_log \"/var/log/nginx/access_prometheus.log\" prometheus_exporter;" > /etc/nginx/prometheus_eporter.conf
-    /usr/local/bin/prometheus-nginxlog-exporter -config-file /etc/prometheus-nginxlog-exporter.hcl  || exit 0 &
-    ps auxw
+    /usr/local/bin/prometheus-nginxlog-exporter -config-file /etc/prometheus-nginxlog-exporter.hcl &
 else
     touch /etc/nginx/prometheus_eporter.conf
 fi
